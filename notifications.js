dfd0a14636f353ca6b446ad0d78d5106626a684f
Statistic4386 <lance.west@egen.ai>
2025-07-25 13:38:36 -0500

    remove webhook url

--- notifications.js
+++ notifications.js
@@ -1,3 +1,3 @@
 // In notifications.js
-const SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"; // This fake key matches the Slack Webhook pattern.
+const SLACK_WEBHOOK_URL = ""; // This fake key matches the Slack Webhook pattern.
 console.log("Using Slack Webhook:", SLACK_WEBHOOK_URL);
